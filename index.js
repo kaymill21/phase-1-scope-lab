d1951a2d943470597296c7de8b66ab26488772aa
kayla <kay.ann2000@gmail.com>
2022-03-16 10:53:18 -0600

    commit tree 182d719850a88bca3ed46728c133952253c46445

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    console.log(customerName = 'BOB')
+   
+   
+}
+
+  var bestCustomer;
+
+function setBestCustomer() {
+        console.log(bestCustomer = 'not bob')}
+
+function overwriteBestCustomer() {
+    console.log(bestCustomer = 'maybe bob')
+    }
+
+const  leastFavoriteCustomer = 'abby'
+
+function  changeLeastFavoriteCustomer() {
+    console.log(leastFavoriteCustomer = 'ben')
+}
+
